a531b333bb08b395554fa9eaecc2e86327cbe0e2
surfing2003 <surfing2003@naver.com>
2020-12-20 17:04:11 +0900

    Thread

--- example_12/app/src/main/java/com/example/example_12/MainActivity.kt
+++ example_12/app/src/main/java/com/example/example_12/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.example_12
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import kotlin.concurrent.thread
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,3 +56,19 @@ class WorkerThread : Thread(){
     }
 }
 
+// 메인 스레드가 아닌 작업스레드에서 UI변경의 작업이 일어나면 오류가 발생
+//class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//        Thread {
+//            var i = 0
+//            while (i < 10){
+//                findViewById<TextView>(R.id.textView).text = "$i"
+//                i += 1
+//                Thread.sleep(1000)
+//            }
+//        }.start()
+//    }
+//}
