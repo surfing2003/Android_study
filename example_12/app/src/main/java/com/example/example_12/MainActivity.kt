8156a2699607a47cc29cf28f93cc749823de432d
surfing2003 <surfing2003@naver.com>
2020-12-20 16:55:07 +0900

    Thread

--- example_12/app/src/main/java/com/example/example_12/MainActivity.kt
+++ example_12/app/src/main/java/com/example/example_12/MainActivity.kt
@@ -3,16 +3,46 @@ package com.example.example_12
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var thread = WorkerThread()
-        thread.start()
+        var thread_0 = WorkerThread()
+        thread_0.start()
+
+        var thread_1 = Thread(WorkerRunnable())
+        thread_1.start()
+
+        Thread {
+            var i = 0
+            while (i < 10){
+                i += 1
+                Log.i("Lambda Thread","$i")
+            }
+        }.start()
+
+        thread(start=true){
+            var i = 0
+            while(i < 10){
+                i += 1
+                Log.i("Kotlin Thread","$i")
+            }
+        }
     }
 }
+class WorkerRunnable : Runnable{
+    override fun run(){
+        var i = 0
+        while(i < 10){
+            i += 1
+            Log.i("Runnable Thread","$i")
+        }
+    }
+}
+
 class WorkerThread : Thread(){
     override fun run(){
         var i = 0
@@ -23,3 +53,4 @@ class WorkerThread : Thread(){
         }
     }
 }
+
